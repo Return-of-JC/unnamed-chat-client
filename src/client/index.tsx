effab97391598a4ba7c687be3eb9178e0506f019
Jesse Garcia <hello@jesseg.co>
2023-01-13 11:05:35 -0700

    consolidated tooling configs into package.json
    I had no idea this was a thing! this makes it a little easier to manage
    now that it's in one file!

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,7 +1,6 @@
 import { render } from 'solid-js/web'
 import { Router, Routes, Route } from '@solidjs/router'
 import { LoginComponent } from '@molecules/login'
-import { createSignal } from 'solid-js'
 
 import './index.scss'
 
